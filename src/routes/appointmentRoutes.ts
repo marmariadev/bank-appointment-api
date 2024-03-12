dd32649f25526cb9569f1220c65478a70bc3964b
marmariadev <marmariadev@gmail.com>
2024-03-12 09:41:51 -0300

    Appointment routes

---
+++ src/routes/appointmentRoutes.ts
@@ -0,0 +1,11 @@
+import { Router } from 'express';
+import * as appointmentController from '../controllers/appointmentController';
+
+const router = Router();
+
+router.get('/', appointmentController.getAllAppointments);
+router.get('/:id', appointmentController.getAppointmentById);
+router.post('/schedule', appointmentController.scheduleAppointment);
+router.put('/cancel/:id', appointmentController.cancelAppointment); // Nota: ':id' para cancelar un turno específico
+
+export default router;

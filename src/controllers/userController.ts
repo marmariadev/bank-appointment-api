c964532c709b2740bf0b13295fe8dfe512f4b6ee
marmariadev <marmariadev@gmail.com>
2024-03-12 11:40:46 -0300

    User controller

--- src/controllers/userController.ts
+++ src/controllers/userController.ts
@@ -1,15 +1,25 @@
 import { Request, Response } from 'express';
+import * as userService from '../services/userService';
 
 export const getAllUsers = (req: Request, res: Response) => {
-  res.send('Obtener todos los usuarios.');
+  const users = userService.getUsers();
+  res.json(users);
 };
 
-export const getUserById = (req: Request, res: Response) => {
-  res.send(`Detalle del usuario ${req.params.id}.`);
+export const getUser = (req: Request, res: Response) => {
+  const user = userService.getUserById(parseInt(req.params.id));
+  if (user) {
+    res.json(user);
+  } else {
+    res.status(404).send('User not found');
+  }
 };
 
 export const registerUser = (req: Request, res: Response) => {
-  res.send('Registro de un nuevo usuario.');
+  const { name, email, birthdate, nDni, username, password } = req.body;
+  // Aquí asumimo que el body ya contiene toda la información necesaria y validada
+  const newUser = userService.createUser(name, email, new Date(birthdate), nDni, username, password);
+  res.status(201).json(newUser);
 };
 
 export const loginUser = (req: Request, res: Response) => {
